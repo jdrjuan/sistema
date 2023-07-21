b7a8c51e17222371746bf9bc32c689a159712d5c
Juan Romero <jdrjuan@gmail.com>
2023-07-21 14:22:50 -0300

    Crea archivo de configuración config.js
    Define:
        bgColor: '#333'
        max: 8
        language: 'es-AR'
        infoEmail: 'info@email.com'
        active: true

---
+++ config.js
@@ -0,0 +1,11 @@
+/* Archivo de configuración */
+
+const config = {
+    bgColor: '#333',
+    max: 8,
+    language: 'es-AR',
+    infoEmail: 'info@email.com',
+    active: true
+};
+
+export default config;
